06ee4b535c08472f63492411a52d200ae55e4362
Russell Jones <jonesrussell42@gmail.com>
2026-04-18 17:30:29 -0400

    build(frontend): disable overlapping vite public dir

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [vue(), tailwindcss()],
+    publicDir: false,
     resolve: {
         alias: { '@': fileURLToPath(new URL('./resources/js', import.meta.url)) },
     },
